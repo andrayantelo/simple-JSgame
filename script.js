9fa9c875ef1cc3dfdbf3797dbd59b3cf24550192
Andrea Anaya <andrayantelo@gmail.com>
2015-01-30 13:31:40 -0600

    moved some code around

--- script.js
+++ script.js
@@ -6,6 +6,59 @@ var playAgain = function () {
        return response;
     }
         
+
+var compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+        console.log("It's a tie!");
+        playAgain()
+    }
+        
+    else if (choice1 === "rock") {
+        if (choice2 === "paper") {
+            console.log("Paper wins!");
+        }
+        else if (choice2 === "scissors") {
+            console.log("Rock wins!");
+        }
+        else if (choice2 === "rope") {
+            console.log("Rope wins!");
+        }
+    }
+    if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            console.log("Paper wins!");
+        }
+        else if (choice2 === "scissors") {
+            console.log("Scissors wins!");
+        }
+        else if (choice2 === "rope") {
+            console.log("Paper wins!");
+        }
+    }
+    if (choice1 === "scissors") {
+        if (choice2 === "rock") {
+            console.log("Rock wins!");
+        }
+        else if (choice2 === "paper") {
+            console.log("Scissors wins!");
+        }
+        else if (choice2 === "rope") {
+            console.log("Scissors wins!");
+        }
+    }
+    if (choice1 === "rope") {
+        if (choice2 === "rock") {
+            console.log("Rope wins!");
+        }
+        else if (choice2 === "paper") {
+            console.log("Paper wins!");
+        }
+        else if (choice2 === "scissors") {
+            console.log("Scissors wins");
+        }
+    }
+};
+
 do {
     var userChoice = prompt("Rock, paper, scissors, or rope?");
 
@@ -25,58 +78,9 @@ do {
     }
 
     console.log("Computer:" + computerChoice);
+    
+    compare(userChoice, computerChoice);
 
-    var compare = function(choice1, choice2) {
-        if (choice1 === choice2) {
-            console.log("It's a tie!");
-            playAgain()
-        }
-        
-        else if (choice1 === "rock") {
-            if (choice2 === "paper") {
-                console.log("Paper wins!");
-            }
-            else if (choice2 === "scissors") {
-                console.log("Rock wins!");
-            }
-            else if (choice2 === "rope") {
-                console.log("Rope wins!");
-            }
-        }
-        if (choice1 === "paper") {
-            if (choice2 === "rock") {
-                console.log("Paper wins!");
-            }
-            else if (choice2 === "scissors") {
-                console.log("Scissors wins!");
-            }
-            else if (choice2 === "rope") {
-                console.log("Paper wins!");
-            }
-        }
-        if (choice1 === "scissors") {
-            if (choice2 === "rock") {
-                console.log("Rock wins!");
-            }
-            else if (choice2 === "paper") {
-                console.log("Scissors wins!");
-            }
-            else if (choice2 === "rope") {
-                console.log("Scissors wins!");
-            }
-        }
-        if (choice1 === "rope") {
-            if (choice2 === "rock") {
-                console.log("Rope wins!");
-            }
-            else if (choice2 === "paper") {
-                console.log("Paper wins!");
-            }
-            else if (choice2 === "scissors") {
-                console.log("Scissors wins");
-            }
-        }
-    };
 while (playAgain === "Y");
 
 
